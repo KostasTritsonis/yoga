73a468983d9a92f53dfc14953ebf2035c2538fa6
KostasTritsonis <ktritsonis@hotmail.com>
2025-01-14 20:25:42 +0200

    14012025

--- src/Components/Benefits.jsx
+++ src/Components/Benefits.jsx
@@ -5,13 +5,13 @@ const Benefits = () => {
   return (
     <div className=' flex justify-evenly p-10 w-full bg-primary text-white text-4xl text-opacity-80 font-gregular'>
       <p>Flexibity</p>
-      <div className='border-white w-1 h-12  bg-white opacity-70'></div>
+      <div className='w-1 h-12  bg-white opacity-70'></div>
       <p>Balance</p>
-      <div className='border-white w-1 h-12  bg-white opacity-70'></div>
+      <div className=' w-1 h-12  bg-white opacity-70'></div>
       <p>Posture</p>
-      <div className='border-white w-1 h-12  bg-white opacity-70'></div>
+      <div className='w-1 h-12  bg-white opacity-70'></div>
       <p>Stress reduction</p>
-      <div className='border-white w-1 h-12  bg-white opacity-70'></div>
+      <div className='w-1 h-12  bg-white opacity-70'></div>
       <p>Better sleep</p>
       
     </div>
